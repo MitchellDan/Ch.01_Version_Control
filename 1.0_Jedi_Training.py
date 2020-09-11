4146f331709cac42c1657a366e3137fe0e999254
MitchellDan <mitchelldan@urbandalescools.com>
2020-09-11 08:36:52 -0500

    Added Drawing

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -4,18 +4,18 @@ Sign your name:Daniel Mitchell
 
 
 Define Forking: 
-
+Make a copy of a repository to your github account
 Define Cloning: 
-
+essentially download
 Define Branching:
-
+Have to simultaneous versions of the same code that can be later put back together
 Define Committing: 
-
+Save changes localy to your clone of the git repo
 Define Merging: 
-
+combine two branches into one
 Define Pushing:
-
+move changes to the verson saved on github
 Define Pull Request:
-
+ask move changes from fork to master
 
 '''
