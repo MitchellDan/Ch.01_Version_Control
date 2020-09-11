7ab1090059f8457a615395a6b1904621ef971fed
MitchellDan <mitchelldan@urbandalescools.com>
2020-09-11 08:30:34 -0500

    Added Drawing

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -7,29 +7,104 @@ The last line keeps the window open until you click to close.
 Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 
 '''
-import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+import turtle #This program makes a minecraft cube.
 
+draw = turtle.Turtle()
+draw.speed(8)
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
-turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
+draw.penup()
+draw.goto(-25,190)
+draw.write("Nice Block")
+draw.goto(0,0)
+draw.pendown() ##Make the Nice block text and rtc
+
+draw.begin_fill() #make right side of block
+draw.fillcolor("#834217")
+draw.goto(0,-150)
+draw.goto(130,-100)
+draw.goto(130,50)
+draw.goto(0,0)
+draw.end_fill()
+
+draw.begin_fill() #make left side of block
+draw.fillcolor("#834217")
+draw.goto(0,-150)
+draw.goto(-130,-100)
+draw.goto(-130,50)
+draw.goto(0,0)
+draw.end_fill()
+
+draw.penup()
+
+draw.goto(-130,50)
+draw.pendown()
+
+draw.begin_fill() #make top side of block
+draw.fillcolor("#0c832c")
+draw.goto(0,100)
+draw.goto(130,50)
+draw.goto(0,0)
+draw.end_fill()
+
+draw.begin_fill() #Add side growth
+draw.goto(0,-20)
+draw.goto(130,30)
+draw.goto(130,50)
+draw.goto(0,0)
+draw.end_fill()
+
+draw.begin_fill() #Add side growth
+draw.goto(0,-20)
+draw.goto(-130,30)
+draw.goto(-130,50)
+draw.goto(0,0)
+draw.end_fill()
+
+draw.penup()
+draw.goto(0,20) #goto bottom of next cube
+
+draw.pendown() #make right side of small cube
+draw.begin_fill()
+draw.fillcolor("#834217")
+draw.goto(90,55)
+draw.goto(90,160)
+draw.goto(0,125)
+draw.goto(0,20)
+draw.end_fill()
+
+draw.begin_fill() #make left side of small cube
+draw.fillcolor("#834217")
+draw.goto(-90,55)
+draw.goto(-90,160)
+draw.goto(0,125)
+draw.goto(0,20)
+draw.end_fill()
+draw.goto(0,125) #rtc
+
+draw.begin_fill() #make top side of small cube
+draw.fillcolor("#0c832c")
+draw.goto(-90,160)
+draw.goto(0,190)
+draw.goto(90,160)
+draw.goto(0,125) #rtc
+draw.end_fill()
+
+draw.begin_fill() #make green side of small cube
+draw.goto(0,110)
+draw.goto(-90,145)
+draw.goto(-90,160)
+draw.goto(0,125)
+draw.end_fill()
+
+draw.begin_fill() #make green side of small cube
+draw.goto(0,110)
+draw.goto(90,145)
+draw.goto(90,160)
+draw.goto(0,125)
+draw.end_fill()
+
+draw.penup()
+draw.goto(130,-130)
+draw.write("Daniel Mitchell")
+
+turtle.Screen().exitonclick()
